d1affc419b84e0bfa83cc140d38166b26af8510a
Kristiyan Dobrev <dobrevkristiyan@gmail.com>
2021-10-27 21:21:26 +0200

    Initial commit 🚀

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update and upgrade
+sudo apt-get update && sudo apt-get upgrade
+
+# Install Docker
+curl -sSL https://get.docker.com | sh
+
+# Add permission to current user
+sudo usermod -aG docker ${USER}
+
+# Install Python
+sudo apt-get install libffi-dev libssl-dev -y
+sudo apt install python3-dev -y
+sudo apt-get install -y python3 python3-pip -y
+
+# Install Docker-Compose
+sudo pip3 install docker-compose
+
+#Enable Docker system service
+sudo systemctl enable docker
